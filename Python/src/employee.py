ae4c31744477e21d5cf39b2e8ed2489a771b1f0e
Thom <22082476@student.hhs.nl>
2024-05-23 16:26:39 +0200

    salesstore error, to little rows

--- Python/src/employee.py
+++ Python/src/employee.py
@@ -10,6 +10,7 @@ def employee ():
     #adventure_employee = adventure_employee()
     #aenc_employee = aenc_employee()
     #northwind_employee = northwind_employee()
+    adventure_employee()
     aenc_employee()
     northwind_employee()
 
@@ -19,13 +20,52 @@ def adventure_employee ():
     adventure_employee = get_data(setup_cursor(os.getenv('adventureworks')), "HumanResources.Employee")
     adventure_payhistory = get_data(setup_cursor(os.getenv('adventureworks')), "HumanResources.EmployeePayHistory")
     adventure_department = get_data(setup_cursor(os.getenv('adventureworks')), "HumanResources.Department")
+    adventure_department_history = get_data(setup_cursor(os.getenv('adventureworks')), "HumanResources.EmployeeDepartmentHistory")
     adventure_countryregion = get_data(setup_cursor(os.getenv('adventureworks')), "Person.CountryRegion")
+    adventure_business_address = get_data(setup_cursor(os.getenv('adventureworks')), "Person.BusinessEntityAddress")
     adventure_address = get_data(setup_cursor(os.getenv('adventureworks')), "Person.Address")
     adventure_stateprovince = get_data(setup_cursor(os.getenv('adventureworks')), "Person.StateProvince")
     adventure_salesterritory = get_data(setup_cursor(os.getenv('adventureworks')), "Sales.SalesTerritory")
+    adventure_salesterritory_history = get_data(setup_cursor(os.getenv('adventureworks')), "Sales.SalesTerritoryHistory")
     adventure_salesstore = get_data(setup_cursor(os.getenv('adventureworks')), "Sales.Store")
     adventure_person = get_data(setup_cursor(os.getenv('adventureworks')), "Person.Person")
     adventure_person_phone = get_data(setup_cursor(os.getenv('adventureworks')), "Person.PersonPhone")
+
+    adventure_salesperson = adventure_salesperson.drop(["rowguid", "ModifiedDate"], axis=1)
+    adventure_employee = adventure_employee.drop(["rowguid", "ModifiedDate"], axis=1)
+    adventure_payhistory = adventure_payhistory.drop(["ModifiedDate"], axis=1)
+    adventure_department = adventure_department.drop(["ModifiedDate"], axis=1)
+    adventure_countryregion = adventure_countryregion.drop(["ModifiedDate"], axis=1)
+    adventure_business_address = adventure_business_address.drop(["rowguid", "ModifiedDate"], axis=1)
+    adventure_address = adventure_address.drop(["rowguid", "ModifiedDate"], axis=1)
+    adventure_stateprovince = adventure_stateprovince.drop(["rowguid", "ModifiedDate"], axis=1)
+    adventure_salesterritory = adventure_salesterritory.drop(["rowguid", "ModifiedDate"], axis=1)
+    adventure_salesstore = adventure_salesstore.drop(["rowguid", "ModifiedDate"], axis=1)
+    adventure_person = adventure_person.drop(["rowguid", "ModifiedDate"], axis=1)
+    adventure_person_phone = adventure_person_phone.drop(["ModifiedDate"], axis=1)
+
+    
+
+    adventure = pd.merge(adventure_person, adventure_employee, on='BusinessEntityID', how='inner')
+    adventure = pd.merge(adventure, adventure_salesperson, on='BusinessEntityID', how='inner')
+    adventure = pd.merge(adventure, adventure_payhistory, on='BusinessEntityID', how='inner')
+    adventure = pd.merge(adventure, adventure_department_history, on='BusinessEntityID', how='inner')
+    adventure = pd.merge(adventure, adventure_department, on='DepartmentID', how='inner')
+    adventure = pd.merge(adventure, adventure_business_address, on='BusinessEntityID', how='inner')
+    adventure = pd.merge(adventure, adventure_address, on='AddressID', how='inner')
+    adventure = pd.merge(adventure, adventure_stateprovince, on='StateProvinceID', how='inner')
+    adventure = pd.merge(adventure, adventure_countryregion, on='CountryRegionCode', how='inner')
+    adventure.rename(columns={"Name": "country"}, inplace=True)
+    adventure = pd.merge(adventure, adventure_salesterritory_history, on='BusinessEntityID', how='inner')
+    adventure = pd.merge(adventure, adventure_salesterritory, on='TerritoryID', how='inner')
+    adventure.rename(columns={"Name": "territory"}, inplace=True)
+    #adventure = pd.merge(adventure, adventure_salesstore, on='BusinessEntityID', how='inner')
+    adventure = pd.merge(adventure, adventure_person_phone, on='BusinessEntityID', how='inner')
+    
+    print(adventure)
+
+
+
 
 def aenc_employee ():
     aenc_employee = get_data(setup_cursor(os.getenv('aenc')), "employee")
@@ -43,7 +83,6 @@ def aenc_employee ():
     aenc.rename(columns={"steet": "address", "zip_code": "postal_code", "dept_name":"department", }, inplace=True)
     aenc.drop(['emp_fname', 'emp_lname', "manager_id", "dept_head_id"], axis=1, inplace=True)
 
-    print(aenc)
     return aenc
 
 
