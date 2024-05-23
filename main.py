3536e5095d4ca093e400afdbfbfbb21706b7786b
BartvBiezen <135812572+BartvBiezen@users.noreply.github.com>
2024-05-23 10:57:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -42,17 +42,10 @@ def products_adventureworks():
     # Som van alle hoeveelheden gepakt per ProductID, kan ook anders
     inventory = inventory.groupby('ProductID')['Quantity'].sum().reset_index()
     
-    # Nog ff checken met het lege CultureID en die naam
-    merge = pd.merge(description_culture, description, how='left')
-    merge2 = pd.merge(merge, culture, left_on='CultureID', right_on='CultureID', how='left')
-    merge2 = merge2.loc[:, ['ProductModelID', 'Description', 'Name']].rename(columns={'Name': 'Culture'})
-    merge2 = merge2.pivot_table(index='ProductModelID', columns='Culture', values='Description', aggfunc='first')
-    merge2.reset_index(inplace=True)
-    merge2 = merge2.rename(columns={'Arabic':'Description_Arabic','Chinese':'Description_Chinese','English':'Description_English','French':'Description_French','Hebrew':'Description_Hebrew','Thai':'Description_Thai'})
-    merge3 = pd.merge(model, merge2, left_on='ProductModelID', right_on='ProductModelID', how='left')
-    merge3 = merge3.loc[:, ['ProductModelID', 'Name', 'CatalogDescription', 'Instructions', 'Description_Arabic','Description_Chinese','Description_English','Description_French','Description_Hebrew','Description_Thai']].rename(columns={'Name': 'ModelName'})
+    # Models
+    merge = model.loc[:, ['ProductModelID', 'Name']].rename(columns={'Name': 'ModelName'})
 
-    product1 = pd.merge(product, merge3, left_on='ProductModelID', right_on='ProductModelID', how='left')
+    product1 = pd.merge(product, merge, left_on='ProductModelID', right_on='ProductModelID', how='left')
     product2 = pd.merge(product1, inventory, left_on='ProductID', right_on='ProductID', how='left')
     product3 = pd.merge(product2, categories, left_on='ProductSubcategoryID', right_on='ProductSubcategoryID', how='left')
    
