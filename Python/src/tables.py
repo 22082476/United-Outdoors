a5015d9a67382f0417b69b44a51b22e96e0572d5
dannynoordamdev <dannynoordam.dev@gmail.com>
2024-05-23 16:17:30 +0200

    commitjuh

--- Python/src/tables.py
+++ Python/src/tables.py
@@ -41,3 +41,8 @@ def aw_sales_territory():
     aw_sales_territory = get_data(cursor_aw, "Sales.SalesTerritory")
     aw_sales_territory = aw_sales_territory.loc[:, ['TerritoryID', 'Name', 'SalesYTD', 'SalesLastYear', 'CostYTD', 'CostLastYear']].rename(columns={'TerritoryID':'sales_territory_id', 'Name':'sales_territory_name', 'SalesYTD':'sales_territory_YTD', 'SalesLastYear':'sales_territory_sales_last_year', 'CostYTD':'sales_territory_cost_YTD', 'CostLastYear':'sales_territory_cost_last_year'})
     return aw_sales_territory
+
+def aw_paymethod_table():
+    aw_paymethod_columns = ['paymethod_id', 'creditcard'].rename(columns={'PayMethodID':'paymethod', 'CreditCard':'creditcard'})
+    aw_paymethod = pd.DataFrame(columns=aw_paymethod_columns)
+    return aw_paymethod
